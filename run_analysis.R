e068c879301d957c00fa31c0e262b363fa8aec62
ShermAnderson <sander66@earthlink.net>
2015-06-20 10:54:35 -0400

    Added Comments

--- run_analysis.R
+++ run_analysis.R
@@ -1,39 +1,105 @@
 
-setwd("C:\\workingOn\\Coursera\\DataCleaning\\DataCleaningProject\\UCI HAR Dataset")
+#
+#    This script will clean the data provided in in the data cleaning project.
+#
+#       There three basic steps
+#           1) Read the training data and create one training data frame
+#           2) Read the test data and create one test data frame
+#           3) Combine the two data sets, add labels and translate activity ids
+#               into activity names.
+#           4) Extract a tidy data set that includes only variables that are
+#               mean or standard deviation caclulations.
 
+# set a working directory and use relative paths for evertything else
+setwd("C:\\workingOn\\Coursera\\DataCleaning\\DataCleaningProject")
 
+#
+# Step1: Read the training data and build one data set
+#
 
-train_subject <- read.table(file="train/subject_train.txt",stringsAsFactors=TRUE, header=FALSE)
-train_x <- read.table(file="train/X_train.txt", header=FALSE)
-train_y <- read.table(file="train/y_train.txt", header=FALSE)
+#   Read the subject identifiers, these are numeric
+train_subject <- read.table(file="UCI HAR Dataset/train/subject_train.txt",
+                            stringsAsFactors=TRUE, header=FALSE)
+
+#   Read the activity codes, these are numeric
+train_y <- read.table(file="UCI HAR Dataset/train/y_train.txt", header=FALSE)
+
+#   Read the raw data
+train_x <- read.table(file="UCI HAR Dataset/train/X_train.txt", header=FALSE)
+
+#   Join together all the columns from all three files inthe order:
+#       subject id, activityid, variables
 trainset<-cbind(train_subject, train_y, train_x)
 
-test_subject <- read.table(file="test/subject_test.txt",stringsAsFactors=TRUE, header=FALSE)
-test_x <- read.table(file="test/X_test.txt", header=FALSE)
-test_y <- read.table(file="test/y_test.txt",  header=FALSE)
+#
+# Step 2: Read the test data and build one data set
+#
+
+#   Read the training subject ids
+test_subject <- read.table(file="UCI HAR Dataset/test/subject_test.txt",
+                           stringsAsFactors=TRUE, header=FALSE)
+
+#   Read the training data activity Ids
+test_y <- read.table(file="UCI HAR Dataset/test/y_test.txt",  header=FALSE)
+
+#   Read the training data raw data
+test_x <- read.table(file="UCI HAR Dataset/test/X_test.txt", header=FALSE)
+
+#   Join together all the columns from all three files inthe order:
+#       subject id, activityid, variables
 testset<-cbind(test_subject, test_y, test_x)
 
+#
+#  Step 3: Build the full data set, add labels and translate activty code to
+#  activity names
+#
+
+#   Build one data frame by combining the rows of the traning and test
+#   data frames
 fulldataset<-rbind(trainset, testset)
 
-features_labels <- read.table(file="features.txt", col.names=c("id","text"), stringsAsFactors=FALSE,header=FALSE)
+#   Read the feature names to use as column labels
+features_labels <- read.table(file="UCI HAR Dataset/features.txt",
+                              col.names=c("id","text"),
+                              stringsAsFactors=FALSE,header=FALSE)
 col_names<-features_labels[,2]
+
+#   Build a vector of colum names that includes the 'Subject' and 'Activity'
+#   columns.
 all_col<-c("Subject", "Activity", features_labels[,2])
 
+#   Apply the column names to the full data set
 colnames(fulldataset)<-all_col
 
-activity_labels <- read.table(file="activity_labels.txt", col.names=c("id","text"), stringsAsFactors=FALSE,header=FALSE)
+#   Read the activity lables, these will be used to translate the activity codes to text.
+activity_labels <- read.table(file="UCI HAR Dataset/activity_labels.txt",
+                              col.names=c("id","text"),
+                              stringsAsFactors=FALSE,header=FALSE)
 
+#   Translate the activity ids to text strings
 for(i in 1:nrow(activity_labels)) {
     fulldataset[fulldataset$Activity == i, 'Activity'] <- activity_labels[i,"text"]
 }
 
+#
+#  Step4: Extract a tidy data set that includes only variables that are
+#               mean or standard deviation caclulations.
+#
 
-cols_needed<-c("Subject", "Activity", all_col[grep("(.*mean.*)|(*.std.*)", all_col, ignore.case=TRUE)])
+# Build a list of columns names that we are going to extract. Use RegEx and
+#  ignore case to be sure we get them all.
+columns_needed<-c("Subject", "Activity",
+                  all_col[grep("(.*mean.*)|(*.std.*)",
+                               all_col,
+                               ignore.case=TRUE)])
 
-final_dataset<-fulldataset[cols_needed]
+#   Create the smaller tidy date set
+tidy_dataset<-fulldataset[columns_needed]
 
-write.table(final_dataset, "C:/workingOn/Coursera/DataCleaning/DataCleaningProject/mean_std.txt", row.names=FALSE)
+#   Write the result to a text file
+write.table(tidy_dataset, "DataCleaningProject_tidy_dataset.txt",
+            row.names=FALSE)
 
 
 
-#body_acc_x_train<- read.table(file="train/Inertial Signals/body_acc_x_train.txt", header=FALSE)
+
